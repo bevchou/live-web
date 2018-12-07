4a5bc2cd3fb728da1264c4f7880f96c48f09373c
bevchou <31548243+bevchou@users.noreply.github.com>
2018-12-07 02:30:04 -0500

    UGH

---
+++ final_test_getText/letterMove.js
@@ -0,0 +1,82 @@
+// https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
+
+
+class letterMove {
+  constructor(mx, my, element) {
+    //mouse position
+    this.mx = mx;
+    this.my = my;
+    //position of element
+    this.x = 0;
+    this.y = 0;
+
+    //element
+    this.element = element;
+    //difference in position
+    this.dx = 0;
+    this.dy = 0;
+
+    //variables
+    this.posData = null;
+  }
+
+  //move in same direciton as mouse velocity
+  //sine wave of position (so it returns back to original position)
+  // 0 to 180 AKA 0 to PI
+  // run() {
+  // this.init();
+  // this.step();
+  // window.requestAnimationFrame(this.step);
+
+  // }
+
+  // init() {
+  // this.posData = this.element.getBoundingClientRect();
+  // this.x = this.posData.x;
+  // this.y = this.posData.y;
+  // //get tangent to movement
+  // this.dx = this.mx - this.x;
+  // this.dy = this.my - this.y;
+  // }
+
+  run() {
+    this.posData = this.element.getBoundingClientRect();
+    this.x = this.posData.x;
+    this.y = this.posData.y;
+    //get tangent to movement
+    this.dx = this.mx - this.x;
+    this.dy = this.my - this.y;
+    let animate = (function() {
+      console.log(this.x);
+      //get animation frame
+      let progressTime = +new Date() - this.startTime;
+      //map 4 seconds of motion to full movement
+      let eqnTime = mapRange(progressTime, 0, 4000, 0, Math.PI);
+      //movement
+      this.x = this.x + this.dx * Math.sin(eqnTime);
+      this.y = this.y + this.dy * Math.sin(eqnTime);
+      this.element.style.position = "absolute";
+      this.element.style.left = this.x;
+      this.element.style.top = this.y;
+
+
+      // this.element.style = this.x + this.dx * Math.sin(eqnTime);
+      //stop animatation when it returns to original position
+      if (eqnTime <= Math.PI) {
+        window.requestAnimationFrame(this.step);
+      }
+    })();
+
+  }
+
+
+}
+
+//https://stackoverflow.com/questions/48802987/is-there-a-map-function-in-vanilla-javascript-like-p5-js
+// linearly maps value from the range (a..b) to (c..d)
+function mapRange(value, a, b, c, d) {
+  // first map value from (a..b) to (0..1)
+  value = (value - a) / (b - a);
+  // then map it from (0..1) to (c..d) and return it
+  return c + value * (d - c);
+}
--- final_test_getText/sketch.js
+++ final_test_getText/sketch.js
@@ -1,18 +1,23 @@
-// links
+// LINKS
 // https://stackoverflow.com/questions/18643766/find-and-replace-specific-text-characters-across-a-document-with-js
 // https://developer.mozilla.org/en-US/docs/Web/API/Node/parentNode
 // https://developer.mozilla.org/en-US/docs/Web/API/Text
 // https://developer.mozilla.org/en-US/docs/Web/API/DocumentOrShadowRoot/elementsFromPoint
 // https://developer.mozilla.org/en-US/docs/Web/API/DocumentOrShadowRoot/elementFromPoint
 
+// GLOBAL VARIABLES
+let letterArray = [];
+
 
 
 window.addEventListener('load', function() {
 
+  //get all text nodes in the webpage
   var textnodes = textNodesUnder(document.body);
   console.log(textnodes);
   for (let i = 0; i < textnodes.length; i++) {
-    let newNode = document.createElement('particles');
+    //new element for each node
+    let newNode = document.createElement('charGroup');
     newNode.innerHTML = textToParticle(textnodes[i].nodeValue);
     textnodes[i].parentNode.replaceChild(newNode, textnodes[i]);
     if (i == textnodes.length - 1) {
@@ -21,32 +26,43 @@ window.addEventListener('load', function() {
     }
   }
 
-  //run this after loop above ends?
+  //modify css of elements
   function changeLetters() {
     document.addEventListener('mousemove', function(e) {
+      //get all elements that collides with mouse
       let activeItems = document.elementsFromPoint(e.x, e.y);
-      // test = activeItems;
-      // console.log(test);
-      let letter = activeItems.find(letter => letter.localName == "particle");
+      //only get the element that is a "character" element
+      let letter = activeItems.find(letter => letter.localName == "character");
       if (letter) {
-        letter.style.color = "red";
+        let posData = letter.getBoundingClientRect();
+        let dx = (e.x - posData.x) * 2;
+        let dy = (e.y - posData.y) * 2;
+
+        letter.style.transform = "translate(" + dx + "px," + dy + "px)";
+
+
+
+        // let currentStyle = window.getComputedStyle(letter, null).getPropertyValue('font-size');
+        // let currentSize = parseFloat(currentStyle);
+        // letter.style.fontSize = currentSize * 1.2 + "px";
+
+        // console.log(currentSize);
       }
     });
   }
 
 
-  //borrowed from https://github.com/plehoux/fontBomb/blob/master/js/explosion.js
-  //look at "explosifyText" function
+  //modified from the "explosifyText" function from FontBomb: https://github.com/plehoux/fontBomb/blob/master/js/explosion.js
+  //breaks text characters into indvidual elements
   function textToParticle(string) {
-    var char, chars, index;
+    var char, chars;
     chars = (function() {
-      var i, len, ref, results;
-      ref = string.split('');
-      results = [];
-      for (index = i = 0, len = ref.length; i < len; index = ++i) {
-        char = ref[index];
+      let ref = string.split('');
+      let results = [];
+      for (let i = 0; i < ref.length; i++) {
+        char = ref[i];
         if (!/^\s*$/.test(char)) {
-          results.push("<particle style='display:inline-block;'>" + char + "</particle>");
+          results.push("<character style='display:inline-block;' class='dostuff animate'>" + char + "</character>");
         } else {
           results.push('&nbsp;');
         }
@@ -55,11 +71,10 @@ window.addEventListener('load', function() {
     })();
     chars = chars.join('');
     chars = (function() {
-      var i, len, ref, results;
-      ref = chars.split('&nbsp;');
-      results = [];
-      for (index = i = 0, len = ref.length; i < len; index = ++i) {
-        char = ref[index];
+      let ref = chars.split('&nbsp;');
+      let results = [];
+      for (let i = 0; i < ref.length; i++) {
+        char = ref[i];
         if (!/^\s*$/.test(char)) {
           results.push("<word style='white-space:nowrap'>" + char + "</word>");
         } else {
@@ -72,16 +87,18 @@ window.addEventListener('load', function() {
   };
 
 
-// https://stackoverflow.com/questions/22921242/remove-carriage-return-and-space-from-a-string
-// https://stackoverflow.com/questions/10730309/find-all-text-nodes-in-html-page
+  // https://stackoverflow.com/questions/22921242/remove-carriage-return-and-space-from-a-string
+  // https://stackoverflow.com/questions/10730309/find-all-text-nodes-in-html-page
+  //gets all the text nodes on the page
   function textNodesUnder(el) {
     //get all the text nodes
-    var n, a = [], b = []
-      walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
+    var n, a = [],
+      b = []
+    walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
     while (n = walk.nextNode()) a.push(n);
     //remove all text nodes with whitespace or carriage return
-    for (let i =0; i < a.length; i++){
-      if (a[i].nodeValue.replace(/[\n\r]+/g, '')){
+    for (let i = 0; i < a.length; i++) {
+      if (a[i].nodeValue.replace(/[\n\r]+/g, '')) {
         b.push(a[i]);
       }
     }
